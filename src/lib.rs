229394043e5741cd7e52a91d2c327b863735dd3b
barrelll <jharrell_@outlook.com>
2018-12-27 14:49:12 -0600

    working on seek impl

--- src/lib.rs
+++ src/lib.rs
@@ -1,153 +1,123 @@
 mod tests;
 
+use std::convert::{From, Into};
+use std::fmt::{Debug, Display};
 use std::io::{Cursor, Error, ErrorKind, Result, Seek, SeekFrom};
+use std::marker::PhantomData;
+use std::ops::{
+    Add, AddAssign, BitAnd, BitAndAssign, BitOr, BitOrAssign, BitXor, BitXorAssign, Div, DivAssign,
+    Mul, MulAssign, Rem, RemAssign, Shl, ShlAssign, Shr, ShrAssign, Sub, SubAssign,
+};
 
 macro_rules! impl_byte {
-    ( $( $x:ty, $y:expr ),* ) => {
+    ( $( $x:ident, $y:expr ),* ) => {
         $(
-            impl Byte for $x {
+            impl Unit for $x {
                 const SIZE: u8 = $y;
-                type SelfType = $x;
 
-                fn add(&mut self, other: Self::SelfType) {
-                    *self += other;
-                }
-                fn sub(&mut self, other: Self::SelfType) {
-                    *self -= other;
-                }
-                fn mul(&mut self, other: Self::SelfType) {
-                    *self *= other;
-                }
-                fn div(&mut self, other: Self::SelfType) {
-                    *self /= other;
-                }
-                fn mdl(&mut self, other: Self::SelfType) {
-                    *self %= other;
-                }
-                fn shr(&mut self, other: Self::SelfType) {
-                    *self >>= other;
-                }
-                fn shl(&mut self, other: Self::SelfType) {
-                    *self <<= other;
-                }
-
-                fn add_ret(self, other: Self::SelfType) -> Option<Self::SelfType> {
-                    self.checked_add(other)
-                }
-                fn sub_ret(self, other: Self::SelfType) -> Option<Self::SelfType> {
-                    self.checked_sub(other)
-                }
-                fn mul_ret(self, other: Self::SelfType) -> Option<Self::SelfType> {
-                    self.checked_mul(other)
-                }
-                fn div_ret(self, other: Self::SelfType) -> Option<Self::SelfType> {
-                    self.checked_div(other)
-                }
-                fn shr_ret(self, other: u32) -> Option<Self::SelfType> {
-                    self.checked_shr(other)
-                }
-                fn shl_ret(self, other: u32) -> Option<Self::SelfType> {
-                    self.checked_shl(other)
-                }
-                fn mdl_ret(self, other: Self::SelfType) -> Self::SelfType {
-                    self % other
+                fn unitfrom(val: u128) -> $x {
+                    val as $x
                 }
             }
         )*
     };
 }
 
-pub trait Byte {
+pub trait Unit:
+    Add<Output=Self>
+    + AddAssign
+    + Sub<Output=Self>
+    + SubAssign
+    + Shl<Output=Self>
+    + ShlAssign
+    + Shr<Output=Self>
+    + ShrAssign
+    + BitAnd<Output=Self>
+    + BitAndAssign
+    + BitOr<Output=Self>
+    + BitOrAssign
+    + BitXor<Output=Self>
+    + BitXorAssign
+    + Mul<Output=Self>
+    + MulAssign
+    + Div<Output=Self>
+    + DivAssign
+    + Rem<Output=Self>
+    + RemAssign
+    + Sized
+    + Copy
+    + Clone
+    + Debug
+    + Display
+{
     const SIZE: u8;
-    type SelfType;
-
-    fn add(&mut self, other: Self::SelfType);
-    fn sub(&mut self, other: Self::SelfType);
-    fn mul(&mut self, other: Self::SelfType);
-    fn div(&mut self, other: Self::SelfType);
-    fn mdl(&mut self, other: Self::SelfType);
-    fn shr(&mut self, other: Self::SelfType);
-    fn shl(&mut self, other: Self::SelfType);
-
-    fn add_ret(self, other: Self::SelfType) -> Option<Self::SelfType>;
-    fn sub_ret(self, other: Self::SelfType) -> Option<Self::SelfType>;
-    fn mul_ret(self, other: Self::SelfType) -> Option<Self::SelfType>;
-    fn div_ret(self, other: Self::SelfType) -> Option<Self::SelfType>;
-    fn shr_ret(self, other: u32) -> Option<Self::SelfType>;
-    fn shl_ret(self, other: u32) -> Option<Self::SelfType>;
-    fn mdl_ret(self, other: Self::SelfType) -> Self::SelfType;
+    fn unitfrom(val: u128) -> Self;
 }
 
 impl_byte!(
     u8, 8, u16, 16, u32, 32, u64, 64, u128, 128, i8, 8, i16, 16, i32, 32, i64, 64, i128, 128
 );
 
-pub trait ByteIterator {
-    type Item;
-    fn max_size() -> u8;
-
-    fn blen(&self) -> usize;
-
-    fn get_slice(&self, x: usize, y: usize) -> &[Self::Item];
+#[derive(Debug, Clone, Copy)]
+pub struct UnitArr<'a, T: Unit> {
+    slice: &'a [T],
 }
 
-impl<T: Byte> ByteIterator for Vec<T> {
-    type Item = T;
-    fn max_size() -> u8 {
+impl<'a, T: Unit> UnitArr<'a, T> {
+    pub fn slice(&self) -> &[T] {
+        self.slice
+    }
+
+    pub fn unit_size() -> u8 {
         T::SIZE
     }
 
-    fn blen(&self) -> usize {
-        self.len()
-    }
-
-    fn get_slice(&self, x: usize, y: usize) -> &[Self::Item] {
-        &self[x..y]
+    pub fn len(&self) -> usize {
+        self.slice.len()
     }
 }
 
-impl<T: Byte> ByteIterator for &[T] {
-    type Item = T;
-    fn max_size() -> u8 {
-        T::SIZE
+impl<'a, T: Unit> From<&'a Vec<T>> for UnitArr<'a, T> {
+    fn from(v: &'a Vec<T>) -> UnitArr<'a, T> {
+        UnitArr { slice: &v[..] }
     }
+}
 
-    fn blen(&self) -> usize {
-        self.len()
-    }
-
-    fn get_slice(&self, x: usize, y: usize) -> &[Self::Item] {
-        &self[x..y]
+impl<'a, T: Unit> From<&'a [T]> for UnitArr<'a, T> {
+    fn from(s: &'a [T]) -> UnitArr<'a, T> {
+        UnitArr { slice: s }
     }
 }
 
 #[derive(Debug, Clone)]
-pub struct BitCursor<T: ByteIterator> {
+pub struct BitCursor<'a, I: Unit, T: Into<UnitArr<'a, I>>> {
     bit_pos: u8,
-    cursor: Cursor<T>,
+    cursor: Cursor<UnitArr<'a, I>>,
+    _marker: PhantomData<&'a T>,
 }
 
-impl<T: ByteIterator> BitCursor<T> {
-    pub fn new(data: T) -> BitCursor<T> {
+impl<'a, I: Unit, T: Into<UnitArr<'a, I>>> BitCursor<'a, I, T> {
+    pub fn new(data: T) -> BitCursor<'a, I, T> {
         BitCursor {
             bit_pos: 0,
-            cursor: Cursor::new(data),
+            cursor: Cursor::new(data.into()),
+            _marker: PhantomData,
         }
     }
 
     pub fn byte_aligned(&self) -> bool {
-        self.bit_pos == (T::max_size() - 1)
+        self.bit_pos == (UnitArr::<I>::unit_size() - 1)
     }
 
-    pub fn into_inner(self) -> T {
+    pub fn into_inner(self) -> UnitArr<'a, I> {
         self.cursor.into_inner()
     }
 
-    pub fn get_ref(&self) -> &T {
+    pub fn get_ref(&self) -> &UnitArr<'a, I> {
         self.cursor.get_ref()
     }
 
-    pub fn get_mut(&mut self) -> &mut T {
+    pub fn get_mut(&mut self) -> &mut UnitArr<'a, I> {
         self.cursor.get_mut()
     }
 
@@ -160,7 +130,7 @@ impl<T: ByteIterator> BitCursor<T> {
     }
 
     pub fn set_bit_pos(&mut self, new: u8) {
-        let max = T::max_size();
+        let max = UnitArr::<I>::unit_size();
         self.bit_pos = new % max;
         self.set_cur_pos((new / max) as u64);
     }
@@ -170,42 +140,52 @@ impl<T: ByteIterator> BitCursor<T> {
     }
 
     pub fn read_u8(&mut self) -> Result<u8> {
-        let curpos = self.cur_position();
-        let bitpos = self.bit_pos;
-        let data = self.cursor.get_ref();
-        let itsize = T::max_size() / 8;
-        for _ in 0..itsize {
-
+        //        let curpos = self.cur_position();
+        //        let bitpos = self.bit_pos;
+        let data = &self.cursor.get_ref().slice()[..];
+        let itsize = UnitArr::<I>::unit_size() / 8;
+        for mut val in &data[0..itsize as usize] {
+            let something = *val * I::unitfrom(8);
+            println!("{:?}", something);
         }
         Ok(0)
     }
 }
 
-impl<T: ByteIterator> Seek for BitCursor<T> {
+impl<'a, I: Unit, T: Into<UnitArr<'a, I>>> Seek for BitCursor<'a, I, T> {
     fn seek(&mut self, pos: SeekFrom) -> Result<u64> {
         // size will always be a byte since we can only do this for Cursor with type &[u8]
         let (base_pos, offset) = match pos {
             SeekFrom::Start(v) => {
-                self.bit_pos = (v % (T::max_size() as u64)) as u8;
-                let seek_to = v / (T::max_size() as u64);
+                let unitsize =  UnitArr::<I>::unit_size() as u64;
+                self.bit_pos = (v % unitsize) as u8;
+                let seek_to = v / unitsize;
                 self.set_cur_pos(seek_to);
                 return Ok(seek_to);
             }
             SeekFrom::Current(v) => {
-                self.bit_pos = (v % (T::max_size() as i64)) as u8;
-                let seek_to = v / (T::max_size() as i64);
-                (self.cur_position(), seek_to)
+                let unitsize =  UnitArr::<I>::unit_size() as i64;
+                let bits = self.bit_pos as i64 + ((self.cur_position() as i64) * unitsize);
+                self.bit_pos = ((v + bits) % (unitsize)) as u8;
+                let seek_to = (v + bits) / unitsize;
+                self.set_cur_pos(seek_to as u64);
+                return Ok(seek_to as u64);
             }
             SeekFrom::End(v) => {
-                self.bit_pos = (v % (T::max_size() as i64)) as u8;
-                let seek_to = v / (T::max_size() as i64);
-                (self.cursor.get_ref().blen() as u64, seek_to)
+                let unitsize =  UnitArr::<I>::unit_size() as i64;
+                self.bit_pos = if v >= 0 {
+                    (v % unitsize) as u8
+                } else {
+                    (v % unitsize).wrapping_neg() as u8
+                };
+                let seek_to = v / unitsize;
+                (self.cursor.get_ref().len() as u64, seek_to)
             }
         };
         let new_pos = if offset >= 0 {
             base_pos.checked_add(offset as u64)
         } else {
-            base_pos.checked_add((offset.wrapping_neg()) as u64)
+            base_pos.checked_sub((offset.wrapping_neg()) as u64)
         };
         match new_pos {
             Some(n) => {
