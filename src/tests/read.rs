f9140ef3ea7e98ccb850c8a4534c8ecdbcf8cc85
barrelll <jharrell_@outlook.com>
2018-12-31 23:46:00 -0600

    added more tests

--- src/tests/read.rs
+++ src/tests/read.rs
@@ -11,4 +11,26 @@ mod u8 {
         assert_eq!(2, amt);
         assert_eq!(vec![46, 128, 0, 0], Vec::from(buf))
     }
+
+    #[test]
+    fn read_from_u8s_to_end() {
+        let data: [u8; 3] = [0b01101010, 0b11110001, 0b01110100];
+        let mut bcurs = BitCursor::new(&data[..]);
+        let _ = bcurs.seek(SeekFrom::Start(13));
+        let mut buf = vec![];
+        let amt = bcurs.read_to_end(&mut buf).unwrap();
+        assert_eq!(2, amt);
+        assert_eq!(vec![46, 128], Vec::from(buf))
+    }
+
+    #[test]
+    fn read_from_u8s_to_string() {
+        let data: [u8; 4] = [0b00011000, 0b10011001, 0b00011001, 0b10011010];
+        let mut bcurs = BitCursor::new(&data[..]);
+        let _ = bcurs.seek(SeekFrom::Start(1));
+        let mut buf = String::new();
+        let amt = bcurs.read_to_string(&mut buf).unwrap();
+        assert_eq!(4, amt);
+        assert_eq!(vec![49, 50, 51, 52], Vec::from(buf))
+    }
 }
