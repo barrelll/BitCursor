a959fcd3fb2a4eb0c4d12c26e05fb3a37672b3c3
barrelll <jharrell_@outlook.com>
2019-01-04 13:06:48 -0600

    removed a test that shouldn't compile anyway

--- src/tests/read.rs
+++ src/tests/read.rs
@@ -61,3 +61,52 @@ mod u8 {
         assert_eq!(7, iter);
     }
 }
+
+mod u16 {
+    use std::io::{Read, Seek, SeekFrom};
+    use BitCursor;
+    #[test]
+    fn read_from_u16s() {
+        let data: [u16; 3] = [0b01101010, 0b11110001, 0b01110100];
+        let mut bcurs = BitCursor::new(&data[..]);
+        let mut buf = vec![0, 0, 0, 0];
+        let amt = bcurs.read(&mut buf).unwrap();
+        assert_eq!(4, amt);
+        assert_eq!(vec![0, 106, 0, 241], Vec::from(buf))
+    }
+
+    #[test]
+    fn read_from_u16s_to_end() {
+        let data: [u16; 3] = [0b01101010, 0b11110001, 0b01110100];
+        let mut bcurs = BitCursor::new(&data[..]);
+        let _ = bcurs.seek(SeekFrom::Start(13));
+        let mut buf = vec![];
+        let amt = bcurs.read_to_end(&mut buf).unwrap();
+        assert_eq!(5, amt);
+        assert_eq!(vec![64, 30, 32, 14, 128], Vec::from(buf))
+    }
+
+    #[test]
+    fn read_from_u16s_to_string() {
+        let data: [u16; 4] = [0b00011000, 0b10011001, 0b00011001, 0b10011010];
+        let mut bcurs = BitCursor::new(&data[..]);
+        let _ = bcurs.seek(SeekFrom::Start(1));
+        let mut buf = String::new();
+        let amt = bcurs.read_to_string(&mut buf).unwrap();
+        assert_eq!(8, amt);
+        assert_eq!(vec![0, 48, 1, 50, 0, 50, 1, 52], Vec::from(buf))
+    }
+
+    #[test]
+    fn read_from_u16_to_bytes() {
+        let data: [u16; 4] = [0b00011000, 0b10011001, 0b00011001, 0b10011011];
+        let mut bcurs = BitCursor::new(&data[..]);
+        let _ = bcurs.seek(SeekFrom::Start(15));
+        let mut iter = 0;
+        for (e, b) in bcurs.bytes().enumerate() {
+            let _ = b.unwrap();
+            iter = e;
+        }
+        assert_eq!(6, iter);
+    }
+}
