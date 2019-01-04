13785fc34428a881f188f7fc0b4f0a3a4c3b38e3
barrelll <jharrell_@outlook.com>
2019-01-04 13:15:24 -0600

    adding tests

--- src/tests/read.rs
+++ src/tests/read.rs
@@ -110,3 +110,101 @@ mod u16 {
         assert_eq!(6, iter);
     }
 }
+
+mod u32 {
+    use std::io::{Read, Seek, SeekFrom};
+    use BitCursor;
+    #[test]
+    fn read_from_u32s() {
+        let data: [u32; 3] = [0b01101010, 0b11110001, 0b01110100];
+        let mut bcurs = BitCursor::new(&data[..]);
+        let mut buf = vec![0, 0, 0, 0];
+        let amt = bcurs.read(&mut buf).unwrap();
+        assert_eq!(4, amt);
+        assert_eq!(vec![0, 0, 0, 106], Vec::from(buf))
+    }
+
+    #[test]
+    fn read_from_u32s_to_end() {
+        let data: [u32; 3] = [0b01101010, 0b11110001, 0b01110100];
+        let mut bcurs = BitCursor::new(&data[..]);
+        let _ = bcurs.seek(SeekFrom::Start(13));
+        let mut buf = vec![];
+        let amt = bcurs.read_to_end(&mut buf).unwrap();
+        assert_eq!(11, amt);
+        assert_eq!(vec![0, 13, 64, 0, 0, 30, 32, 0, 0, 14, 128], Vec::from(buf))
+    }
+
+    #[test]
+    fn read_from_u32s_to_string() {
+        let data: [u32; 4] = [0b00011000, 0b10011001, 0b00011001, 0b10011010];
+        let mut bcurs = BitCursor::new(&data[..]);
+        let _ = bcurs.seek(SeekFrom::Start(1));
+        let mut buf = String::new();
+        let amt = bcurs.read_to_string(&mut buf).unwrap();
+        assert_eq!(16, amt);
+        assert_eq!(vec![0, 0, 0, 48, 0, 0, 1, 50, 0, 0, 0, 50, 0, 0, 1, 52], Vec::from(buf))
+    }
+
+    #[test]
+    fn read_from_u32_to_bytes() {
+        let data: [u32; 4] = [0b00011000, 0b10011001, 0b00011001, 0b10011011];
+        let mut bcurs = BitCursor::new(&data[..]);
+        let _ = bcurs.seek(SeekFrom::Start(15));
+        let mut iter = 0;
+        for (e, b) in bcurs.bytes().enumerate() {
+            let _ = b.unwrap();
+            iter = e;
+        }
+        assert_eq!(14, iter);
+    }
+}
+
+mod u64 {
+    use std::io::{Read, Seek, SeekFrom};
+    use BitCursor;
+    #[test]
+    fn read_from_u64s() {
+        let data: [u64; 3] = [0b01101010, 0b11110001, 0b01110100];
+        let mut bcurs = BitCursor::new(&data[..]);
+        let mut buf = vec![0, 0, 0, 0];
+        let amt = bcurs.read(&mut buf).unwrap();
+        assert_eq!(4, amt);
+        assert_eq!(vec![0, 0, 0, 0], Vec::from(buf))
+    }
+
+    #[test]
+    fn read_from_u64s_to_end() {
+        let data: [u64; 3] = [0b01101010, 0b11110001, 0b01110100];
+        let mut bcurs = BitCursor::new(&data[..]);
+        let _ = bcurs.seek(SeekFrom::Start(13));
+        let mut buf = vec![];
+        let amt = bcurs.read_to_end(&mut buf).unwrap();
+        assert_eq!(23, amt);
+        assert_eq!(vec![0, 0, 0, 0, 0, 13, 64, 0, 0, 0, 0, 0, 0, 30, 32, 0, 0, 0, 0, 0, 0, 14, 128], Vec::from(buf))
+    }
+
+    #[test]
+    fn read_from_u64s_to_string() {
+        let data: [u64; 4] = [0b00011000, 0b10011001, 0b00011001, 0b10011010];
+        let mut bcurs = BitCursor::new(&data[..]);
+        let _ = bcurs.seek(SeekFrom::Start(1));
+        let mut buf = String::new();
+        let amt = bcurs.read_to_string(&mut buf).unwrap();
+        assert_eq!(32, amt);
+        assert_eq!(vec![0, 0, 0, 0, 0, 0, 0, 48, 0, 0, 0, 0, 0, 0, 1, 50, 0, 0, 0, 0, 0, 0, 0, 50, 0, 0, 0, 0, 0, 0, 1, 52], Vec::from(buf))
+    }
+
+    #[test]
+    fn read_from_u64_to_bytes() {
+        let data: [u64; 4] = [0b00011000, 0b10011001, 0b00011001, 0b10011011];
+        let mut bcurs = BitCursor::new(&data[..]);
+        let _ = bcurs.seek(SeekFrom::Start(15));
+        let mut iter = 0;
+        for (e, b) in bcurs.bytes().enumerate() {
+            let _ = b.unwrap();
+            iter = e;
+        }
+        assert_eq!(30, iter);
+    }
+}
