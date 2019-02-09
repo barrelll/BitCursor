a29802218b7efd91911bfcae4537e73102bc510e
barrelll <jharrell_@outlook.com>
2019-02-09 14:37:31 -0600

    begginning work on write tests yay

--- src/tests/write.rs
+++ src/tests/write.rs
@@ -1,1 +1,16 @@
-
+mod u8 {
+    use std::io::{Seek, SeekFrom, Write};
+    use {BitCursor};
+    #[test]
+    fn write() {
+        let mut data: [u32; 3] = [
+            0b10010010001001011000100001101010,
+            0b10010010001001011000100001101010,
+            0b10010010001001011000100001101010,
+        ];
+        let mut bcurs = BitCursor::new(&mut data[..]);
+        let _ = bcurs.seek(SeekFrom::Start(6));
+        let buf = [1];
+        bcurs.write(&buf);
+    }
+}
